df69a7dfaa0e3c13f62c030a1719a1a99e94943d
Daniel Saucedo Avila <st4587@utr.edu.mx>
2026-07-16 18:05:58 -0600

    jwt verification and auth

---
+++ src/routes/usersRoute.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import { getUsers, updateUser, deleteUser, registerUser, loginUser } from '../controller/robustAuthentication.js';
+import { verifyToken } from '../middleware/authMiddleware.js';
+
+const router = express.Router();
+
+// Rutas publicas
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+
+// Rutas protegidas
+router.get('/users', verifyToken, getUsers);
+router.put('/:id', verifyToken, updateUser);
+router.delete('/:id', verifyToken, deleteUser);
+//En el caso de DELETE no solamente es borrar el usuario y ya
+//Lo debe buscar por su token. Si el token que se busca no coincide con el del usuario por borrar, NO LO BORRA
+//Esto por seguridad y/o evitar *accidentes borrando otro usuario
+
+export default router;
